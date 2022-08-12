5d1fcf8586d4d958a6f630faf753d15e31801c61
Ariel Trigo <a.silveiratrigo@gmail.com>
2022-08-12 20:44:55 -0300

    teste

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome='Ariel';
+let sobrenome='Silveira Trigo';
+let anonascimento= 1991;
+let anoatual= 2022;
+let idade= anoatual - anonascimento;
+let cidade= 'Caratinga';
+console.log=`Eu sou o ${nome} ${sobrenome} tenho ${idade}anos e moro em ${cidade}` ;
